421160daf358ff9662b81b39d3b94507c9334b1f
jiweisb23 <70188120+jiweisb23@users.noreply.github.com>
2020-09-24 18:39:58 -0700

    My submission for assignment 2

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,39 @@
-## Put comments here that give an overall description of what your
-## functions do
+## My functions create a special matrix which allow you to calculate & then cache the matrix and it's inverse
 
-## Write a short comment describing this function
 
-makeCacheMatrix <- function(x = matrix()) {
+## This function creates the special matrix which stores functions allowing you to interact with cached objects
 
+makeCacheMatrix <- function(mat = matrix()) {
+  i<-NULL
+  set<-function(y){
+    mat<<-y
+    i<<-NULL
+  }
+  get<-function()mat
+  setinv<-function(solve)i <<-solve
+  getinv<-function()i
+  list(set = set, get = get,
+       setinv=setinv,
+       getinv=getinv)
 }
 
 
-## Write a short comment describing this function
+## This function creates the cached inverse of your matrix if you haven't already created it
 
-cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+cacheSolve <- function(mat, ...) {
+  ## Return a matrix that is the inverse of 'x'
+  i<-mat$getinv()
+  
+  #I<-diag(1,nrow=dim(mat$get())[1], ncol=dim(mat$get())[2])
+  
+  #Check if null
+  if(!is.null(i) ){#  && dim(mat$get())[1]==dim(i)[1] && sameSquareMatrix(mat$get()%*%i,I)){
+    message("getting cached data")
+    return(i)
+  }
+  #if inv null then better real calculate it
+  data<-mat$get()
+  i<-solve(data,...)
+  mat$setinv(i)
+  i
 }
